15a3c786496865cd7baefb975882ff0ef593eade
NCFCBot <NCFCBOT@hotmail.com>
2016-07-06 13:58:49 +0200

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -178,7 +178,7 @@ API.getWaitListPosition = function(id){
     var botCreatorIDs = [];
 
     var basicBot = {
-        version: "2.2-NCFC",
+        version: "2.3-NCFC",
         status: false,
         name: "BotX",
         loggedInID: null,
