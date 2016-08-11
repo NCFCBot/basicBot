464bdeb5b3d09a9ab28b204e1363fa0cadb59eea
NCFCBot <NCFCBOT@hotmail.com>
2016-08-11 13:48:54 +0200

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -633,15 +633,18 @@ API.getWaitListPosition = function(id){
                 }
             },
             changeDJCycle: function () {
-            var toggle = $(".cycle-toggle");
-            if(API.getWaitList().length > 12) {
-                if (!toggle.hasClass("enabled")) {
+                var toggle = $(".cycle-toggle");
+                if (toggle.hasClass("enabled")) {
                     toggle.click();
+                    if (basicBot.settings.cycleGuard) {
+                        basicBot.room.cycleTimer = setTimeout(function () {
+                            if (toggle.hasClass("disabled")) toggle.click();
+                        }, basicBot.settings.cycleMaxTime * 60 * 1000);
+                    }
                 }
-            }
-            if(API.getWaitList().length < 10) {
-                if (toggle.hasClass("disabled")) {
+                else {
                     toggle.click();
+                    clearTimeout(basicBot.room.cycleTimer);
                 }
             },
             intervalMessage: function () {
