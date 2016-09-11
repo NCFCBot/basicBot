75552c6bb5d08cff9abc3d668a3cb9690659bebe
NCFCBot <NCFCBOT@hotmail.com>
2016-09-11 14:32:16 +0200

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -204,7 +204,7 @@ API.getWaitListPosition = function(id){
             timeGuard: true,
             maximumSongLength: 10,
             autodisable: true,
-            commandCooldown: 10,
+            commandCooldown: 3,
             usercommandsEnabled: true,
             lockskipPosition: 3,
             lockskipReasons: [
